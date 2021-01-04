4a446e02a5e029cba313107eba2af2de116f5971
rarconaut <68246130+rarconaut@users.noreply.github.com>
2021-01-04 12:57:09 -0800

    Working on developing Flask API routes.

--- Rawaf/app.py
+++ Rawaf/app.py
@@ -1,50 +1,34 @@
-"""Initialize Flask app."""
-from flask import Flask
+# """Initialize Flask app."""
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from ddtrace import patch_all
 
 
-db = SQLAlchemy()
-patch_all()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{username}:{password}@localhost/{database}"
+db = SQLAlchemy(app)
 
 
-def create_app():
-    """Construct the core application."""
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object("config.Config")
-
-    db.init_app(app)
-
-    with app.app_context():
-        from . import routes  # Import routes
-
-        db.create_all()  # Create database tables for our data models
-
-        return app
-        
-
-
-"""Data models."""
+# """Data models."""
 from . import db
 
 
-class User(db.Model):
-    """Data model for user accounts."""
+# # """Example table"""
+# class User(db.Model):
 
-    __tablename__ = "flasksqlalchemy-tutorial-users"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=False, unique=True, nullable=False)
-    email = db.Column(db.String(80), index=True, unique=True, nullable=False)
-    created = db.Column(db.DateTime, index=False, unique=False, nullable=False)
-    bio = db.Column(db.Text, index=False, unique=False, nullable=True)
-    admin = db.Column(db.Boolean, index=False, unique=False, nullable=False)
+    # __tablename__ = "flasksqlalchemy-tutorial-users"
+    # id = db.Column(db.Integer, primary_key=True)
+    # username = db.Column(db.String(64), index=False, unique=True, nullable=False)
+    # email = db.Column(db.String(80), index=True, unique=True, nullable=False)
+    # created = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+    # bio = db.Column(db.Text, index=False, unique=False, nullable=True)
+    # admin = db.Column(db.Boolean, index=False, unique=False, nullable=False)
 
-    def __repr__(self):
-        return "<User {}>".format(self.username)
+    # def __repr__(self):
+    #     return "<User {}>".format(self.username)
 
 
 
-"""Application routes."""
+# """Application routes."""
 from datetime import datetime as dt
 
 from flask import current_app as app
@@ -52,27 +36,50 @@ from flask import make_response, redirect, render_template, request, url_for
 
 from .models import User, db
 
+# Create a different route for each element's API (built from querying our database) 
+# - these will be the source URLs for our visualizations' .js files 
+@app.route("/api")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/map<br/>"
+        f"/api/v1.0/hex<br/>"
+        f"/api/v1.0/graph"
+    )
 
-@app.route("/", methods=["GET"])
-def user_records():
-    """Create a user via query string parameters."""
-    username = request.args.get("user")
-    email = request.args.get("email")
-    if username and email:
-        existing_user = User.query.filter(
-            User.username == username or User.email == email
-        ).first()
-        if existing_user:
-            return make_response(f"{username} ({email}) already created!")
-        new_user = User(
-            username=username,
-            email=email,
-            created=dt.now(),
-            bio="In West Philadelphia born and raised, \
-            on the playground is where I spent most of my days",
-            admin=False,
-        )  # Create an instance of the User class
-        db.session.add(new_user)  # Adds new User record to database
-        db.session.commit()  # Commits all changes
-        redirect(url_for("user_records"))
-    return render_template("users.jinja2", users=User.query.all(), title="Show Users")
+
+@app.route("/api/v1.0/map", methods=["GET"])
+def mapAPI():
+
+@app.route("/api/v1.0/hex", methods=["GET"])
+def hexAPI():
+
+@app.route("/api/v1.0/graph", methods=["GET"])
+def graphAPI():
+
+
+
+# @app.route("/", methods=["GET"])
+# def user_records():
+#     # """Create a user via query string parameters."""
+#     username = request.args.get("user")
+#     email = request.args.get("email")
+#     if username and email:
+#         existing_user = User.query.filter(
+#             User.username == username or User.email == email
+#         ).first()
+#         if existing_user:
+#             return make_response(f"{username} ({email}) already created!")
+#         new_user = User(
+#             username=username,
+#             email=email,
+#             created=dt.now(),
+#             bio="In West Philadelphia born and raised, \
+#             on the playground is where I spent most of my days",
+#             admin=False,
+#         )  # Create an instance of the User class
+#         db.session.add(new_user)  # Adds new User record to database
+#         db.session.commit()  # Commits all changes
+#         redirect(url_for("user_records"))
+#     return render_template("users.jinja2", users=User.query.all(), title="Show Users")
